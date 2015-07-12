7e567fb960eeff5ff1d3e7cdc45f2e47dc9131d4
Sebastien Peek <sebastien.peek@gmail.com>
2015-07-12 20:24:16 +1000

    Updated playground introduction stuff.

--- swift-algorithms.playground/Contents.swift
+++ swift-algorithms.playground/Contents.swift
@@ -1,7 +1,17 @@
 /*:
 ### Introduction
 
+Due to not having gone to University, I figured it was best that I gained some more knowledge around algorithms and how to implement them etc. Thus, when the Princeton University Algorithm course became available on Coursera, I figured why the hell shouldn't I do it?
+
+This playground will be split up into weeks, and then into chapters explicitly.
+
+### Week 1
+
+Union-Find
 */
+
+// Quick Find
+
 let quickFindUF = QuickFindUF(N: 10)
 quickFindUF.union(1, q: 2)
 quickFindUF.union(5, q: 6)
@@ -12,3 +22,5 @@ quickFindUF.connected(1, q: 5)
 quickFindUF.union(3, q: 9)
 quickFindUF.union(3, q: 8)
 quickFindUF.connected(8, q: 9)
+
+// Quick Union
