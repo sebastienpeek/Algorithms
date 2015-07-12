5b57d0840fa2143ce4a21b72e0a887fddbb9ee87
Sebastien Peek <sebastien.peek@gmail.com>
2015-07-12 21:33:22 +1000

    Weighted Quick Union implementation.

--- swift-algorithms.playground/Contents.swift
+++ swift-algorithms.playground/Contents.swift
@@ -15,12 +15,12 @@ let quickFindUF = QuickFindUF(N: 10)
 quickFindUF.union(1, q: 2)
 quickFindUF.union(5, q: 6)
 quickFindUF.union(2, q: 5)
-
 quickFindUF.connected(1, q: 5)
 
 quickFindUF.union(3, q: 9)
-quickFindUF.union(3, q: 8)
-quickFindUF.connected(8, q: 9)
+quickFindUF.union(8, q: 3)
+quickFindUF.union(3, q: 7)
+quickFindUF.connected(7, q: 9)
 
 // Quick Union
 
@@ -28,8 +28,27 @@ let quickUnionUF = QuickUnionUF(N: 10)
 quickUnionUF.union(1, q: 2)
 quickUnionUF.union(5, q: 6)
 quickUnionUF.union(2, q: 5)
-quickUnionUF.connected(5, q: 1)
+quickUnionUF.connected(1, q: 5)
 
 quickUnionUF.union(3, q: 9)
-quickUnionUF.union(3, q: 8)
-quickUnionUF.connected(3, q: 9)
+quickUnionUF.union(8, q: 3)
+quickUnionUF.union(3, q: 7)
+quickUnionUF.connected(7, q: 9)
+
+// Quick Union Improvements
+
+let weightedQuickUnionUF = WeightedQuickUnionUF(N: 10)
+weightedQuickUnionUF.id
+weightedQuickUnionUF.sz
+
+weightedQuickUnionUF.union(1, q: 2)
+weightedQuickUnionUF.union(5, q: 6)
+weightedQuickUnionUF.union(2, q: 5)
+weightedQuickUnionUF.connected(1, q: 5)
+
+weightedQuickUnionUF.id
+
+weightedQuickUnionUF.union(3, q: 9)
+weightedQuickUnionUF.union(8, q: 3)
+weightedQuickUnionUF.union(3, q: 7)
+weightedQuickUnionUF.connected(7, q: 9)
