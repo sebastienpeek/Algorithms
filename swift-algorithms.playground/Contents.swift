c810466118c222345d2cd6c422b7beea8cec6df6
Sebastien Peek <sebastien.peek@gmail.com>
2015-07-12 20:19:04 +1000

    Implemented Quick Find for Week 1's Union-Find chapter.

--- swift-algorithms.playground/Contents.swift
+++ swift-algorithms.playground/Contents.swift
@@ -1,6 +1,14 @@
-//: Playground - noun: a place where people can play
+/*:
+### Introduction
 
-import Foundation
+*/
+let quickFindUF = QuickFindUF(N: 10)
+quickFindUF.union(1, q: 2)
+quickFindUF.union(5, q: 6)
+quickFindUF.union(2, q: 5)
 
-var str = "Hello, playground"
+quickFindUF.connected(1, q: 5)
 
+quickFindUF.union(3, q: 9)
+quickFindUF.union(3, q: 8)
+quickFindUF.connected(8, q: 9)
--- swift-algorithms.playground/Sources/Week 1/Union-Find/Quick Find/QuickFind.swift
+++ swift-algorithms.playground/Sources/Week 1/Union-Find/Quick Find/QuickFind.swift
@@ -1,2 +1,31 @@
 import Foundation
 
+public class QuickFindUF {
+    
+    public var id = Dictionary<Int, Int>()
+    
+    public init(N:Int) {
+        for i in 0...N {
+            id.updateValue(i, forKey: i)
+        }
+    }
+    
+    public func connected(p:Int, q:Int)->Bool {
+        return (id[p] == id[q])
+    }
+    
+    public func union(p:Int, q:Int) {
+        
+        let pid:Int = id[p]!
+        let qid:Int = id[q]!
+        
+        for i in 0...id.count {
+            if (id[i] == pid) {
+                id.updateValue(qid, forKey: i)
+            }
+        }
+        
+    }
+    
+}
+
